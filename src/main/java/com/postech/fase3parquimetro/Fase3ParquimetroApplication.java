1f64bc3ddfcf8258969bb7f781b94af2074afcaf
mayaravlima <rm349362@fiap.com.br>
2023-10-29 14:28:07 -0300

    first commit

---
+++ src/main/java/com/postech/fase3parquimetro/Fase3ParquimetroApplication.java
@@ -0,0 +1,13 @@
+package com.postech.fase3parquimetro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fase3ParquimetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fase3ParquimetroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postech/fase3parquimetro/Fase3ParquimetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postech.fase3parquimetro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fase3ParquimetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
